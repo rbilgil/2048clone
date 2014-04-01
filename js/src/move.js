8c5c2ccce92e8b49bf72607616ffb24f9a54ab57
Robin Bilgil <robin@parsly.co.uk>
2014-04-01 13:07:18 +0100

    Reimplemented left movement again to scan from right to left

--- js/src/move.js
+++ js/src/move.js
@@ -24,79 +24,35 @@ function handleCollision(boxNumber, num) {
 }
 
 function moveLeft() {
+
     for (var xPos = 2; xPos <= boxCount; xPos++) {
-        movementX(xPos, 1);
-    }
-}
 
-function moveRight() {
-    for (var xPos = boxCount - 1; xPos >= 1; xPos--) {
-        movementX(xPos, boxCount);
-    }
-}
+        for (var i = 0; i < activeNums.length; i++) {
 
-function moveUp() {
-    for (var yPos = 2; yPos <= boxCount; yPos++) {
-        movementY(yPos, 1);
-    }
-}
+            var num = activeNums[i];
 
-function moveDown() {
-    for (var yPos = boxCount - 1; yPos >= 1; yPos--) {
-        movementY(yPos, boxCount);
-    }
-}
+            if (xPos === num.boxX) {
 
-function movementX(xPos, startingPos) {
+                var newXPos;
 
-    for (var i = 0; i < activeNums.length; i++) {
-        var num = activeNums[i];
-        if (num.boxX === xPos) {
-
-            var newXPos = startingPos;
-            var boxNumber;
-
-            while (isBoxFull(boxNumber = getBoxNumber(newXPos, num.boxY))) {
-                if (newXPos !== num.boxX
-                    && handleCollision(boxNumber, num)) {
-                    break;
-                } else if (newXPos > num.boxX) {
-                    newXPos--;
-                } else if (newXPos < num.boxX) {
-                    newXPos++;
-                } else {
-                    break;
+                while (!isBoxFull(getBoxNumber(newXPos  = num.boxX - 1, num.boxY))
+                    && newXPos >= 1) {
+                    num.setBoxX(newXPos);
                 }
+
+                var collisionNum = getNumInBox(getBoxNumber(num.boxX - 1, num.boxY));
+
+                if (collisionNum.value === num.value) {
+                    num.value += collisionNum.value;
+                    num.setBoxX(collisionNum.boxX);
+                    deactivate(collisionNum);
+                }
+
             }
 
-            num.setBoxX(newXPos);
         }
+
     }
-}
 
-function movementY(yPos, startingPos) {
 
-    for (var i = 0; i < activeNums.length; i++) {
-        var num = activeNums[i];
-        if (num.boxY === yPos) {
-
-            var newYPos = startingPos;
-            var boxNumber;
-
-            while (isBoxFull(boxNumber = getBoxNumber(num.boxX, newYPos))) {
-                if (newYPos !== num.boxY
-                    && handleCollision(boxNumber, num)) {
-                    break;
-                } else if (newYPos > num.boxY) {
-                    newYPos--;
-                } else if (newYPos < num.boxY) {
-                    newYPos++;
-                } else {
-                    break;
-                }
-            }
-
-            num.setBoxY(newYPos);
-        }
-    }
 }
